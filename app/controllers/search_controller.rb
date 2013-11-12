1bcd8aa634f50a1dab2897ed35ab610f47e3cc77
taiyib <taiyib_ashraf@hotmail.com>
2013-11-12 15:30:33 -0800

    Styling changes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,22 +12,22 @@ class SearchController < ApplicationController
       return
     end
 
-    # rs = [
-    #   {
-    #     title:  'The Wynn Golf Club',
-    #     desc:   'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    #     lat:    36.124981,
-    #     lng:    -115.157932,
-    #   },
-    #   {
-    #     title:  'Las Vegas National Golf Club',
-    #     desc:   'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.',
-    #     lat:    36.129556,
-    #     lng:    -115.125786,
-    #   },
-    # ]
+    rs = [
+       {
+         title:  'The Wynn Golf Club',
+         desc:   'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+         lat:    36.124981,
+         lng:    -115.157932,
+       },
+       {
+         title:  'Las Vegas National Golf Club',
+         desc:   'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.',
+         lat:    36.129556,
+         lng:    -115.125786,
+       },
+    ]
 
-    rs = Need.all.map(&:to_search)
+    #rs = Need.all.map(&:to_search)
 
     render json: rs
   end
