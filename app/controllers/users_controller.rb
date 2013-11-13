f6511155ff651281df2a076a8289dfca79cb250e
Carson McMillan <carsonm@gmail.com>
2013-11-12 21:33:03 -0800

    asdf;lkjasd;fljk

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
 	def attend
-    attendance = Attendance.find_or_create_by(user_id: params[:user_id], need_id: params[:need_id])
+    attendance = Attendance.find_or_create_by(user_id: params[:id], need_id: params[:need_id])
     redirect_to :back
 	end
 
 	def remove
-	  attendance = Attendance.find_or_create_by(user_id: params[:user_id], need_id: params[:need_id])
+	  attendance = Attendance.find_or_create_by(user_id: params[:id], need_id: params[:need_id])
 	  attendance.destroy
     redirect_to :back
   end
